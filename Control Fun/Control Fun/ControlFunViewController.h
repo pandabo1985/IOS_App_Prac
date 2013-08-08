f6d164fb8cce9cb44ac5f1fafaa7823b606f8546
panda <pandabo1985@gmail.com>
2013-08-08 23:12:36 +0800

    iPhone控件的学习
    我是一只蜗牛，慢慢往上爬。

--- Control Fun/Control Fun/ControlFunViewController.h
+++ Control Fun/Control Fun/ControlFunViewController.h
@@ -8,6 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ControlFunViewController : UIViewController
+#define kSwitchesSegmentIndex 0
+
+@interface ControlFunViewController : UIViewController <UIAccelerometerDelegate>
+{
+    UITextField *nameField;
+    UITextField *numberField;
+    UILabel *sliderLable;
+    
+    UISwitch *leftSwitch;
+    UISwitch *rightSwitch;
+    UIButton *doSomeThingButton;
+    
+}
+
+@property(nonatomic, retain) IBOutlet UITextField *nameField;
+@property(nonatomic, retain) IBOutlet UITextField *numberField;
+@property(nonatomic, retain) IBOutlet UILabel *sliderLable;
+@property(nonatomic, retain) IBOutlet UISwitch *leftSwitch;
+@property(nonatomic, retain) IBOutlet UISwitch *rightSwitch;
+@property(nonatomic, retain) IBOutlet UIButton *doSomeThingButton;
+ 
+-(IBAction)textFieldDoneEditDoneEditing:(id)sender;
+-(IBAction)backgroundTap:(id)sender;
+-(IBAction)sliderChanged:(id)sender;
+-(IBAction)toggleControls:(id)sender;
+-(IBAction)switchChanged:(id)sender;
+-(IBAction)buttonPressed;
 
 @end
--- Control Fun/Control Fun/ControlFunViewController.m
+++ Control Fun/Control Fun/ControlFunViewController.m
@@ -14,6 +14,83 @@
 
 @implementation ControlFunViewController
 
+@synthesize nameField;
+@synthesize numberField;
+@synthesize sliderLable;
+@synthesize leftSwitch;
+@synthesize rightSwitch;
+@synthesize doSomeThingButton;
+
+-(IBAction)textFieldDoneEditDoneEditing:(id)sender
+{
+    [sender resignFirstResponder];
+}
+-(IBAction)backgroundTap:(id)sender
+{
+    [numberField resignFirstResponder];
+    [nameField resignFirstResponder];
+}
+
+-(IBAction)sliderChanged:(id)sender
+{
+    UISlider *slider = (UISlider *)sender;
+    int progressAsInt = (int)(slider.value + 0.5f);
+    NSString *newText = [[NSString alloc] initWithFormat:@"%d", progressAsInt];
+    sliderLable.text = newText;
+    [newText release];
+}
+
+-(IBAction)toggleControls:(id)sender
+{
+    if ([sender selectedSegmentIndex] == kSwitchesSegmentIndex) {
+        leftSwitch.hidden = NO;
+        rightSwitch.hidden = NO;
+        doSomeThingButton.hidden = YES;
+    }else
+    {
+        leftSwitch.hidden = YES;
+        rightSwitch.hidden = YES;
+        doSomeThingButton.hidden = NO;
+    }
+}
+
+-(IBAction)switchChanged:(id)sender
+{
+    UISwitch *whitchSWitch = (UISwitch *)sender;
+    BOOL setting = whitchSWitch.isOn;
+    [leftSwitch setOn:setting animated:YES];
+    [rightSwitch setOn:setting animated:YES];
+}
+
+-(IBAction)buttonPressed
+{
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Are you sure?" delegate:self cancelButtonTitle:@"No Way!" destructiveButtonTitle:@"Yes, I'm Sure!" otherButtonTitles:nil];
+    [actionSheet showInView:self.view];
+    [actionSheet release];
+    
+  
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    
+    if (buttonIndex !=[actionSheet cancelButtonIndex]) {
+        NSString *msg = nil;
+        if (nameField.text.length > 0) {
+            msg = [[NSString alloc] initWithFormat:@"You can breathe easy, %@, everything went OK.",nameField.text];
+        }else
+        {
+            msg = @"You can breath easy, everything went Ok.";
+        }
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something was done" message:msg delegate:self cancelButtonTitle:@"Phew" otherButtonTitles: nil];
+        [alert show];
+        [alert release];
+        [msg release];
+        
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -24,6 +101,17 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)dealloc
+{
+    [nameField release];
+    [numberField release];
+    [sliderLable release];
+    [leftSwitch release];
+    [rightSwitch release];
+    [doSomeThingButton release];
+    [super dealloc];
 }
 
 @end
