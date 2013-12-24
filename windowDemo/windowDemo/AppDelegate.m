52b95f564b9d06ae19d654af4721bf5d0667c8fa
panda <pandabo1985@gmail.com>
2013-12-24 22:39:55 +0800

    UIWindow & UIWindowLevel笔记
    http://www.cnblogs.com/smileEvday/archive/2012/03/27/2420362.html
    参考实验发现，设置arc会到之window被迅速回收掉而没有显示。不设置，黄色的window也没有显示出来，很是奇怪。

--- windowDemo/windowDemo/AppDelegate.m
+++ windowDemo/windowDemo/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+/**
+ *级别的高低顺序从小到大为Normal < StatusBar < Alert，下面
+ */
 
 @implementation AppDelegate
 
@@ -14,8 +17,29 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor yellowColor];
     [self.window makeKeyAndVisible];
+
+    UIWindow *normalWindow = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    normalWindow.backgroundColor = [UIColor blueColor];
+    normalWindow.windowLevel = UIWindowLevelNormal;
+    [normalWindow makeKeyAndVisible];
+    
+    CGRect windowRect = CGRectMake(100, 100, [[UIScreen mainScreen] bounds].size.width -100, [[UIScreen mainScreen] bounds].size.width -100);
+    UIWindow *alertLevelWindow = [[UIWindow alloc] initWithFrame:windowRect];
+    alertLevelWindow.windowLevel = UIWindowLevelAlert;
+    alertLevelWindow.backgroundColor = [UIColor redColor];
+    [alertLevelWindow makeKeyAndVisible];
+    
+    UIWindow *statusLevelWindow = [[UIWindow alloc] initWithFrame:CGRectMake(0, 50, [[UIScreen mainScreen] bounds].size.width, 20)];
+    statusLevelWindow.windowLevel = UIWindowLevelStatusBar;
+    statusLevelWindow.backgroundColor = [UIColor blackColor];
+    [statusLevelWindow makeKeyAndVisible];
+    
+    
+    NSLog(@"normal window level : %f",UIWindowLevelNormal);
+    NSLog(@"alert window level : %f",UIWindowLevelAlert);
+    NSLog(@"status window level : %f",UIWindowLevelStatusBar);
     return YES;
 }
 
