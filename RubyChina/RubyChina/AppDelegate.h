e74f3190ba4e0fb39e506c3aba41846b61074688
panda <pandabo1985@gmail.com>
2014-02-10 21:15:50 +0800

    rubychina iPhone客户端学习
    一点点学习，rubychina客户端

---
+++ RubyChina/RubyChina/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RubyChina
+//
+//  Created by pan dabo on 14-2-10.
+//  Copyright (c) 2014年 pan dabo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
